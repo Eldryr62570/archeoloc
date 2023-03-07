d2ee7023ed18ab9ca8e68716a48ba2073219bcbf
TeddyH08 <teddy.hemonet@gmail.com>
2023-03-07 22:17:52 +0100

    tests

--- asset/js/photo.js
+++ asset/js/photo.js
@@ -98,7 +98,7 @@
       context.fillRect(0, 0, canvas.width, canvas.height);
   
       const data = canvas.toDataURL("image/png");
-      photo.setAttribute("src", data);
+      photo.setAttribute("value", data);
     }
   
     // On capture une photo en récupérant le contenu courant de la
--- photo.php
+++ photo.php
@@ -12,12 +12,15 @@
     <div class="bg-gradient-to-b from-[#DA9D56] to-white">
         <div class="contentarea">
             <div class="camera flex flex-col justify-center items-center">
-                <video id="video" class="w-full h-[80vh]">Le flux vidéo n'est pas disponible.</video>
-                <button id="startbutton">Prendre une photo</button>
+                <video id="video" class="w-full h-[70vh]">Le flux vidéo n'est pas disponible.</video>
+                <button id="startbutton" class="rounded-full bg-white w-20 h-20"></button>
             </div>
             <canvas id="canvas"> </canvas>
             <div class="output">
-                <img id="photo" alt="L'image capturée apparaîtra ici.">
+                <!-- <img id="photo" alt="L'image capturée apparaîtra ici."> -->
+                <form action="asset/php/photo.php" method="post">
+                    <input type="image" id="photo">
+                </form>
             </div>
             <p>
                 Consultez l'article <a href="https://developer.mozilla.org/fr/docs/Web/API/WebRTC_API/Taking_still_photos">Prendre des photos avec <code>getUserMedia()</code></a> pour en savoir plus sur les technologies utilisées ici.
