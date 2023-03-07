77674a2c16d0459775b0f69867c7242849b9d0d8
TeddyH08 <teddy.hemonet@gmail.com>
2023-03-07 20:55:17 +0100

    test

--- photo.php
+++ photo.php
@@ -1,18 +1,30 @@
-<div class="contentarea">
-  <p>
-    Wow Dans cet exemple, on illustre comment utiliser un flux média provenant de la webcam, en récupérer une image et créer un PNG à partir de celle-ci pour l'afficher sur la page.
-  </p>
-  <div class="camera">
-    <video id="video">Le flux vidéo n'est pas disponible.</video>
-    <button id="startbutton">Prendre une photo</button>
-  </div>
-  <canvas id="canvas"> </canvas>
-  <div class="output">
-    <img id="photo" alt="L'image capturée apparaîtra ici.">
-  </div>
-  <p>
-    Consultez l'article <a href="https://developer.mozilla.org/fr/docs/Web/API/WebRTC_API/Taking_still_photos">Prendre des photos avec <code>getUserMedia()</code></a> pour en savoir plus sur les technologies utilisées ici.
-  </p>
-</div>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ArchéoPlan - Photo</title>
 
-<script src="asset/js/photo.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="bg-gradient-to-b from-[#DA9D56] to-white">
+        <div class="contentarea">
+            <div class="camera flex flex-col justify-center items-center">
+                <video id="video" class="w-full h-[80vh]">Le flux vidéo n'est pas disponible.</video>
+                <button id="startbutton">Prendre une photo</button>
+            </div>
+            <canvas id="canvas"> </canvas>
+            <div class="output">
+                <img id="photo" alt="L'image capturée apparaîtra ici.">
+            </div>
+            <p>
+                Consultez l'article <a href="https://developer.mozilla.org/fr/docs/Web/API/WebRTC_API/Taking_still_photos">Prendre des photos avec <code>getUserMedia()</code></a> pour en savoir plus sur les technologies utilisées ici.
+            </p>
+        </div>
+    </div>
+
+    <script src="asset/js/photo.js"></script>
+</body>
+</html>
