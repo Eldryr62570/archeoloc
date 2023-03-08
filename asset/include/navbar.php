f5db6467d50ed2802494c3c171d81368ce6ab53a
TeddyH08 <teddy.hemonet@gmail.com>
2023-03-08 10:26:19 +0100

    Navbar

---
+++ asset/include/navbar.php
@@ -0,0 +1,11 @@
+<div class="shadow-[0_0_10px_0.2px_rgba(0,0,0,0.3)] fixed bg-white w-full py-2 bottom-0 z-10 flex flex-row justify-around items-center px-4">
+    <a href=""><img src="asset/img/Navbar/accueil.png" alt=""></a>
+    <div class="w-px h-5 bg-black rounded"></div>
+    <a href=""><img src="asset/img/Navbar/accueil.png" alt=""></a>
+    <div class="w-px h-5 bg-black rounded"></div>
+    <a href=""><img src="asset/img/Navbar/accueil.png" alt=""></a>
+    <div class="w-px h-5 bg-black rounded"></div>
+    <a href=""><img src="asset/img/Navbar/accueil.png" alt=""></a>
+    <div class="w-px h-5 bg-black rounded"></div>
+    <a href=""><img src="asset/img/Navbar/accueil.png" alt=""></a>
+</div>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <?php include("asset/include/navbar.php") ?>
+
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, ducimus, distinctio saepe quis inventore, nulla numquam tenetur quae quo adipisci doloribus! Ex molestiae tempore veritatis iusto. Nisi earum aspernatur ducimus!
+    Labore quidem unde dolore, nobis ab sit rerum dicta alias, enim temporibus architecto accusantium consectetur quos, earum tenetur sequi? Molestiae, aspernatur! Inventore ipsam, vel soluta corporis quaerat delectus tenetur ea.
+    Dolorem similique ullam, atque rerum aperiam harum! Qui doloribus enim quae molestias non odit inventore ratione ipsum adipisci sed itaque provident tenetur, aspernatur doloremque eum quasi, expedita rerum vero in.
+    Perspiciatis at, consequuntur fuga exercitationem esse ex unde temporibus qui voluptate earum neque minima quam dolores adipisci minus dolorem repellendus quis cupiditate laboriosam dolorum, veritatis, mollitia voluptatibus consectetur! Pariatur, ea?
+    Neque, debitis aliquid esse quia excepturi distinctio odit officia laborum doloremque! Necessitatibus ducimus corrupti ex corporis. Amet ullam ea, illum, magni molestias nemo facilis repellendus nesciunt quae alias laboriosam facere.
+    Perferendis pariatur sint nostrum laudantium ipsum neque sequi ducimus numquam magnam! Reiciendis ea eius ipsa, officiis nesciunt reprehenderit voluptate quidem accusantium adipisci at aliquam est. Nobis qui ad officiis sapiente?
+    Sit et odio repellat, officia obcaecati labore facilis? Cupiditate possimus sequi error quae voluptatibus temporibus fuga nulla deserunt reprehenderit dolorum ratione eius ad, esse quos cum commodi delectus nesciunt nostrum?
+    Ea, fuga explicabo debitis ut magnam ab natus voluptates sapiente dolor id assumenda inventore facilis enim consectetur maiores aliquam neque rem ipsum saepe doloremque itaque fugit illum! Ducimus, laboriosam recusandae!
+    Illum facere possimus quis eos, cum animi quo vitae aliquid cupiditate voluptatum! Quia, consectetur doloremque tempora sapiente optio dolores totam! Animi minima quos quidem quas vero. Illo cum quidem natus!
+    Quibusdam ab eos est tenetur porro perferendis, saepe debitis qui unde, corrupti quaerat dolorem necessitatibus voluptatem perspiciatis cumque iste sunt, reprehenderit eveniet dolor laboriosam tempora error rerum repellendus. Nulla, earum?
+    Architecto cupiditate sint quis non fugiat esse deserunt aliquam distinctio perferendis possimus. Amet quia nisi veniam molestias alias natus maiores saepe repudiandae vel commodi quam, quasi soluta suscipit eum? Numquam.
+    Perferendis unde hic, commodi inventore autem dolore quam odit quis consequuntur earum dolor qui repudiandae distinctio quibusdam possimus ratione veritatis neque libero ipsa excepturi fuga expedita temporibus. Corrupti, et laudantium?
+    Quod, saepe laudantium vero non, minus ex cum, animi dolorem qui consectetur voluptas. Tenetur earum ipsam inventore, ullam unde, impedit ex, illum repudiandae dolore neque odio eum sed qui omnis.
+    Natus quis, dicta numquam dolorum, dolores possimus nesciunt est porro consequuntur quam cumque rem nulla eius minima earum deserunt ratione et fugiat expedita perferendis veritatis eum. A sunt suscipit ipsum.
+    Iste sint necessitatibus consequuntur, molestiae provident blanditiis suscipit recusandae et repellat quas velit ab, repellendus, exercitationem a libero illum temporibus! Odio nesciunt at in modi illo! Placeat hic voluptates consequuntur?
+    Quibusdam quod quaerat ex praesentium saepe nulla beatae nostrum cumque ut temporibus, neque voluptate, vitae et sed error officiis dolore commodi omnis est quam ab, tempora magnam dolorem eius. Corporis?
+    Voluptate quibusdam nesciunt, dolorum quia iure, saepe architecto incidunt perferendis quidem aspernatur quae at. Maxime, optio enim. Eligendi perspiciatis nulla quisquam labore quis, reiciendis atque iusto cupiditate architecto dicta eaque!
+    Illum consequatur corporis asperiores soluta quisquam repudiandae omnis aspernatur nostrum. Deleniti ipsum, dignissimos laboriosam cum nesciunt dolorum doloribus blanditiis iure voluptatibus nostrum? Atque incidunt impedit reprehenderit in tempora ea earum!
+    Quisquam aperiam, dolorum placeat minus vel debitis? Labore dicta, mollitia quis laborum ut fugiat dolores voluptas? Laudantium non assumenda facilis totam odio commodi veniam necessitatibus tenetur explicabo, inventore quibusdam! Molestiae!
+    Quibusdam assumenda adipisci minus! Magnam esse commodi ducimus architecto quae qui rem illo veritatis nobis eaque, tempora soluta magni deserunt vel adipisci nihil officiis omnis. Animi, enim maxime? Ea, eius?</p>
+</body>
+</html>
